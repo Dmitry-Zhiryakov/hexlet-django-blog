2e4ae6174a8c5ce26a3ea8724020747df805a213
Dmitry Zhiryakov <dmitry_zhiryakov@mail.ru>
2023-04-19 13:30:29 +0300

    Update hexlet_django_blog/views.py and hexlet_django_blog/article/views.py

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
 
 
-def index(request):
-    name = 'Article'
-    return render(request, 'articles.html', context={'name': name},)
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse('Article')
