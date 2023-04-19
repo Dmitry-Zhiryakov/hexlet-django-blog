4ac70e1f8976fefdbb4181b42e16ce2f8b9425e4
Dmitry Zhiryakov <dmitry_zhiryakov@mail.ru>
2023-04-19 17:58:54 +0300

    Update hexlet_django_blog/article/urls.py, hexlet_django_blog/article/views.py, hexlet_django_blog/views.py

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -3,5 +3,5 @@ from django.views import View
 
 
 class IndexView(View):
-    def get(self, request, *args, **kwargs):
-        return HttpResponse('Article')
+    def get(self, request, tags, article_id):
+        return HttpResponse(f"Статья номер {article_id}. Тег {tags}")
