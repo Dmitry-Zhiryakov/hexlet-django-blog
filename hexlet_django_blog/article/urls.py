75058b39fe1301162041e4ee8a539dc07d6309ae
Dmitry Zhiryakov <dmitry_zhiryakov@mail.ru>
2023-05-02 16:09:33 +0300

    Add the ability to update an article

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from hexlet_django_blog.article.views import IndexView, ArticleView, ArticleFormCreateView
+from hexlet_django_blog.article.views import IndexView, ArticleView, ArticleFormCreateView, ArticleFormEditView
 
 
 urlpatterns = [
+    path('<int:id>/edit/', ArticleFormEditView.as_view(), name='articles_update'),
     path('', IndexView.as_view(), name='articles_index'),
     path('<int:id>/', ArticleView.as_view(), name='article_detail'),
     path('create/', ArticleFormCreateView.as_view(), name='articles_create'),
