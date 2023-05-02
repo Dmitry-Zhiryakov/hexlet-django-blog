4ec43daa44eea1efe001c5ab953586d06d513ae9
Dmitry Zhiryakov <dmitry_zhiryakov@mail.ru>
2023-05-02 13:05:31 +0300

    add add the ability to create an article

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 from hexlet_django_blog.article.models import Article
+from django.forms import ModelForm
+
+
+class ArticleForm(ModelForm):
+    class Meta:
+        model = Article
+        fields = ['name', 'body']
 
 
 class IndexView(View):
@@ -19,3 +26,17 @@ class ArticleView(View):
         return render(request, 'articles/show.html', context={
             'article': article,
         })
+
+
+class ArticleFormCreateView(View):
+
+    def get(self, request, *args, **kwargs):
+        form = ArticleForm()
+        return render(request, 'articles/create.html', {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('articles_index')
+        return render(request, 'articles/create.html', {'form': form})
